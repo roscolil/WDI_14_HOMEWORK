cab6158f2307b6a679ad7cd6c565343c395e12cf
roscolil <ross.lillis@gmail.com>
2017-11-30 17:26:02 +1100

    added alamein loop

--- Ross/wk01/4-thu/pt_planner.js
+++ Ross/wk01/4-thu/pt_planner.js
@@ -1,36 +1,27 @@
-// Declare origin and destination
-var origin
-var destination
+// Declare origin and destination and print to console
+var origin = 'Richmond';
+var destination = 'Hawthorn';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Print origin and destination to console
 console.log('Origin: ' + origin);
 console.log('Destination: ' + destination);
 
-// Print route to console
+// Declare an array for each train line
+var alameinLine = ['Flinders Street', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie'];
+var glenWaverlyLine = ['Flagstaff', 'Melbourne Central', 'Parliament', 'Richmond', 'Kooyong', 'Tooronga'];
+var SandringhamLine = ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran', 'Windsor'];
 
-// Print how many stops to console
+var originIndex = alameinLine.indexOf(origin);
+var destIndex = alameinLine.indexOf(destination) + 1;
+var stationStr = alameinLine.slice(originIndex, destIndex).toString();
+var stops = Math.abs(destIndex - originIndex - 1);
+
+
+for (var i = 0; i <= alameinLine.length; i++) {
+    console.log(stationStr + ' --------> ');
+}
+console.log(stops + ' stops total.');
+
+
+
+
+// Convert array to string and compare to 'Richmond' using 'else if'??
