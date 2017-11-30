e6a0059b9688d1502c99f19baf45f544da103154
roscolil <ross.lillis@gmail.com>
2017-11-30 15:33:16 +1100

    initial commit

--- Ross/wk01/3-wed/main.js
+++ Ross/wk01/3-wed/main.js
@@ -1,10 +1,24 @@
-var days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-var lastElement = days_of_the_week.pop();
-days_of_the_week.unshift(lastElement);
+//var days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var days_of_the_week = 'mon tue wed thur fri sat sun'.split(' ');
+
+//var lastElement = days_of_the_week.pop();
+//days_of_the_week.unshift(lastElement);
+days_of_the_week.unshift(days_of_the_week.pop());
+
 
 var new_days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', ['Saturday', 'Sunday']];
 
 var removeWeekend = new_days_of_the_week.splice(0, 5);
 
 removeWeekend.sort();
+
+
+
+var newArray = [['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],['Saturday', 'Sunday']];
+
+var removeArray = newArray.pop();
+
+removeArray[0].reverse();
+
+console.log(removeArray);
---
+++ Ross/wk01/4-thu/pt_planner.js
@@ -0,0 +1,36 @@
+// Declare origin and destination
+var origin
+var destination
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Print origin and destination to console
+console.log('Origin: ' + origin);
+console.log('Destination: ' + destination);
+
+// Print route to console
+
+// Print how many stops to console
