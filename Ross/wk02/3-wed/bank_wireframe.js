5bddada3bcbfc6f0a9ee576017bf815f7c1d7332
roscolil <ross.lillis@gmail.com>
2017-12-07 19:58:23 +1100

    committing first 4 points

--- Ross/wk02/3-wed/bank_wireframe.js
+++ Ross/wk02/3-wed/bank_wireframe.js
@@ -1,35 +1,42 @@
 var savingsBalance = document.querySelector('.savings_balance');
-var checkingBalance = document.querySelector('.checking _balance');
+var checkingBalance = document.querySelector('.checking_balance');
 var depositButtonSave = document.querySelector('.deposit_button_save');
 var withdrawButtonSave = document.querySelector('.withdraw_button_save');
 var depositButtonCheck = document.querySelector('.deposit_button_check');
 var withdrawButtonCheck = document.querySelector('.withdraw_button_check');
 var savingsInput = document.querySelector('.savings_in');
 var checkingInput = document.querySelector('.checking_in');
+var savingsResult;
+var depositResult;
 
 var depositSavings = function () {
-  var savingsResult = Number(savingsInput.value);
+  savingsResult = Number(savingsInput.value);
   savingsBalance.textContent = savingsResult;
+  savingsInput.value = '';
 };
 
 depositButtonSave.addEventListener('click', depositSavings);
 
 var withdrawSavings = function () {
-  var savingsResult = savingsResult - Number(savingsInput.value);
+  savingsResult = savingsResult - Number(savingsInput.value);
   savingsBalance.textContent = savingsResult;
+  savingsInput.value = '';
 };
 
 withdrawButtonSave.addEventListener('click', withdrawSavings);
 
 var depositChecking = function () {
-  var depositResult = Number(checkingInput.value);
+  depositResult = Number(checkingInput.value);
   checkingBalance.textContent = depositResult;
+  checkingInput.value = '';
 };
 
 depositButtonCheck.addEventListener('click', depositChecking);
 
 var withdrawChecking = function () {
-  var depositResult = depositResult - Number(checkingInput.value);
+  depositResult = depositResult - Number(checkingInput.value);
+  checkingBalance.textContent = depositResult;
+  checkingInput.value = '';
 };
 
 withdrawButtonCheck.addEventListener('click', withdrawChecking);
