baaa015f403002954c3a7d8cf5ce5f0a022609f9
roscolil <ross.lillis@gmail.com>
2017-12-08 16:16:21 +1100

    final commit

--- Ross/wk02/3-wed/bank_wireframe.js
+++ Ross/wk02/3-wed/bank_wireframe.js
@@ -1,48 +1,49 @@
-// Things to do
-// Format currency output
-// Turn input field to red on $0
-
-
-var savingsBalance = document.querySelector('.savings_balance');
-var checkingBalance = document.querySelector('.checking_balance');
+var savingsBalance = document.querySelector('span');
+var checkingBalance = document.querySelector('span');
 var depositButtonSave = document.querySelector('.deposit_button_save');
 var withdrawButtonSave = document.querySelector('.withdraw_button_save');
 var depositButtonCheck = document.querySelector('.deposit_button_check');
 var withdrawButtonCheck = document.querySelector('.withdraw_button_check');
 var savingsInput = document.querySelector('.savings_in');
 var checkingInput = document.querySelector('.checking_in');
-// var savingsZero = savingsInput.style.background = 'red';
-// var checkinZero = checkingInput.style.background = 'red';
-var savingsResult;
-var depositResult;
+var savingsResult = 0;
+var checkingResult = 0;
 
 var depositSavings = function () {
-  savingsResult = Number(savingsInput.value);
+  savingsResult = savingsResult + Number(savingsInput.value);
   savingsBalance.textContent = savingsResult;
+  savingsInput.style.backgroundColor = 'lightgrey';
   savingsInput.value = '';
 };
 
 var withdrawSavings = function () {
-    if (savingsResult > savingsInput.value) {
-      savingsResult = savingsResult - Number(savingsInput.value);
-  } else {
-      savingsBalance.textContent = savingsResult;
-      savingsInput.value = '';
+  if (savingsResult >= savingsInput.value) {
+    savingsResult = savingsResult - Number(savingsInput.value);
+    savingsBalance.textContent = savingsResult;
+    savingsInput.value = '';
+  }
+
+  if (savingsResult === 0) {
+  savingsInput.style.backgroundColor = 'red';
   }
 };
 
 var depositChecking = function () {
-  depositResult = Number(checkingInput.value);
-  checkingBalance.textContent = depositResult;
+  checkingResult = checkingResult + Number(checkingInput.value);
+  checkingBalance.textContent = checkingResult;
+  checkingInput.style.backgroundColor = 'lightgrey';
   checkingInput.value = '';
 };
 
 var withdrawChecking = function () {
     if (depositResult > checkingInput.value) {
-      depositResult = depositResult - Number(checkingInput.value);
-  } else {
+      checkingResult = depositResult - Number(checkingInput.value);
       checkingBalance.textContent = depositResult;
       checkingInput.value = '';
+  }
+
+  if (checkingResult === 0) {
+    checkingInput.style.backgroundColor = 'red';
   }
 };
 
