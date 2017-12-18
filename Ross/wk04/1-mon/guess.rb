72ec2d1ee51cfe8fefc49463b9a5e921cd9da29c
roscolil <ross.lillis@gmail.com>
2017-12-18 20:50:37 +1100

    final commit

--- Ross/wk04/1-mon/guess.rb
+++ Ross/wk04/1-mon/guess.rb
@@ -2,7 +2,7 @@
 # Guess the number
 
 # You are to generate a basic "guess my number" game. The computer will pick a random number
-#between 0 and 10. The user will guess the number until they guess correctly.
+# between 0 and 10. The user will guess the number until they guess correctly.
 
 # Specification:
 # The user should be asked to guess a number
@@ -11,14 +11,21 @@
 # Extensions:
 # Let the user choose the maximum value (so they can play a long game with a random value between 0 and 10000, for example).
 # Give feedback to the user: "Wrong, guess higher!" or "Wrong, guess lower!"
+puts 'Choose a maximum value:'
+range = gets.chomp.to_i
 
-random_number = rand(11)
-puts ("Guess a number between 1 and 10")
+random_number = rand(range) + 1
+puts "Guess a number between 1 and #{range}:"
 guess = gets.chomp.to_i
 
 while guess != random_number
-  puts ("Wrong guess. Try again.")
-  guess = gets.chomp.to_i
+  if guess < random_number
+    puts 'Too low. Try again.'
+    guess = gets.chomp.to_i
+  else
+    puts 'Too high. Try again.'
+    guess = gets.chomp.to_i
+  end
 end
 
-  puts ("You guessed correctly")
+puts 'You guessed correctly'
