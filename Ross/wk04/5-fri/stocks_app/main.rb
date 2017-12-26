8872255e4d29479395d9209b0ea463686c7b1f17
roscolil <ross.lillis@gmail.com>
2017-12-26 12:49:40 +1100

    start of method

--- Ross/wk04/5-fri/stocks_app/main.rb
+++ Ross/wk04/5-fri/stocks_app/main.rb
@@ -1,12 +1,24 @@
 # ruby gem declarations
+require "pry"
 require "sinatra"
 require "sinatra/reloader"
 require "stock_quote"
 
 
+
 get '/' do
   stock = params["stock"]
-  @quote = StockQuote::Stock.quote("aapl")
+  quote_response = StockQuote::Stock.quote("aapl")
+
+
+  def get_stock_info(stock)
+    stock.each do | stock |
+      return @stock
+    end
+  end
+
+  # binding.pry
+  # puts 'end'
 
   erb(:index)
 end
