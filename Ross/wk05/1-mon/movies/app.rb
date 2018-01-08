7f35d58279cc9d403406e3080cb427f95ed17098
roscolil <ross.lillis@gmail.com>
2018-01-08 16:16:52 +1100

    displaying info

--- Ross/wk05/1-mon/movies/app.rb
+++ Ross/wk05/1-mon/movies/app.rb
@@ -15,9 +15,16 @@ get '/search_result' do
   apikey = "apikey=2f6435d9"
   movie = params[:title]
   result = HTTParty.get("http://omdbapi.com/?#{apikey}&t=#{movie}").parsed_response
+
   @title = result["Title"]
-  @year = result["Year"]
   @rated = result["Rated"]
   @released = result["Released"]
+  @director = result["Director"]
+  @language = result["Language"]
+  @awards = result["Awards"]
+  @poster = result["Poster"]
+  @imdb_rating = result["imdbRating"]
+
+  erb :search_result
 
 end
