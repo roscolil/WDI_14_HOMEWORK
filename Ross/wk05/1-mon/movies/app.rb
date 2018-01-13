5eff21c4c35d5f951cdcefbc1749450cf7c80f27
roscolil <ross.lillis@gmail.com>
2018-01-13 14:14:08 +1100

    changed some syntax

--- Ross/wk05/1-mon/movies/app.rb
+++ Ross/wk05/1-mon/movies/app.rb
@@ -9,18 +9,16 @@ get '/' do
   erb :index
 end
 
-get '/about' do
-  erb :about
-end
 
 get '/search_result' do
   @apikey = "apikey=2f6435d9"
-  @movie = params[:title]
+  @movie = params[:title]           # Passed in from form on index.erb
   result = HTTParty.get("http://omdbapi.com/?#{@apikey}&s=#{@movie}").parsed_response
-  @search = result["Search"]
+  @search = result["Search"]        # Index 'Search' returns array for list in search_result.erb
   erb :search_result
 end
 
+# Details page for selected title
 get '/movie_result' do
   movie_result = HTTParty.get("http://omdbapi.com/?apikey=2f6435d9&i=#{params[:id]}").parsed_response
 
@@ -35,13 +33,18 @@ get '/movie_result' do
   @poster = movie_result["Poster"]
   @imdb_rating = movie_result["imdbRating"]
   @plot = movie_result["Plot"]
-  # @imdbID = movie_result["Search"][0]["imdbID"]
-  conn = PG.connect(dbname: 'movies_db')
 
-  if "SELECT EXISTS (SELECT 1 FROM movies_cache WHERE imdbid = '#{params[:id]}');" == 't'
-    sql = "SELECT title, rated, genre, released, director FROM movies_cache;"
-    conn.exec(sql)
-    conn.close
+  conn = PG.connect(dbname: 'movies_db')
+  sql_query = "SELECT EXISTS (SELECT 1 FROM movies_cache WHERE imdbid = '#{params[:id]}');"
+  conn.exec(sql_query)
+  conn.close
+
+# Condition always true, even if == 't' or 'f' could be sql_query
+  if sql_query == 't'
+    # conn = PG.connect(dbname: 'movies_db')
+    # sql = "SELECT title, rated, genre, released, director FROM movies_cache;"
+    # conn.exec(sql)
+    # conn.close
 
     erb :cached_result
   else
@@ -60,6 +63,10 @@ end
 
 get '/cached_result' do
   erb :cached_result
+end
+
+get '/about' do
+  erb :about
 end
 
 get '/history' do
