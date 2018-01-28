86694269e322b433a656108964b32348faebb5ec
roscolil <ross.lillis@gmail.com>
2018-01-28 11:54:09 +1100

    attempted to show background images but didnt work

--- Ross/wk07/4-thu/drop_down/main.js
+++ Ross/wk07/4-thu/drop_down/main.js
@@ -1,10 +1,27 @@
-
 var cities = ["NYC", "SF", "LA", "SYD", "ATX"];
 var select = document.querySelector('select');
 
 for (var i = 0; i < cities.length; i++) {
   var option = document.createElement('option');
   option.innerHTML = cities[i];
-  // option.value =
   select.appendChild(option);
 }
+
+// switch (select) {
+//   case cities[0]:
+//     document.('select')[0].classList.add('.nyc');
+//     break;
+//   case cities[1]:
+//     document.querySelector('select')[1].classList.add('.sf');
+//     break;
+//   case cities[2]:
+//     document.querySelector('select')[2].classList.add('.ls');
+//     break;
+//   case cities[3]:
+//     document.querySelector('select')[3].classList.add('.sydney');
+//     break;
+//   case cities[4]:
+//     document.querySelector('select')[4].classList.add('.austin');
+//     break;
+//   default:
+// }
