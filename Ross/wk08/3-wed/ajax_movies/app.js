17bec6d7d13e2e5c691ac98399c724aa15e75406
roscolil <ross.lillis@gmail.com>
2018-01-31 17:01:08 +1100

    input tag working

--- Ross/wk08/3-wed/ajax_movies/app.js
+++ Ross/wk08/3-wed/ajax_movies/app.js
@@ -1,25 +1,24 @@
-var options = {
-  url: 'http://www.omdbapi.com/?apikey=2f6435d9&s=jaws'
-};
 
-var $list = $('.container');
-
-$.ajax(options).done(function(response){
-  response.Search.forEach(function(movie){
-    var $newParaElem = $('<p>');
-    var $newH2Elem = $('<h2>');
-    var $newImageElem = $('<img>');
-    $newParaElem.append($newH2Elem);
-    $newH2Elem.append($newImageElem);
-    $newH2Elem.text(movie.Title);
-    $newImageElem.text(movie.Poster);
-    $container.append($newParaElem);
-  });
-});
-
+var $container = $('.container');
 
 $('button').on('click', function() {
-  var $newChildName = $('input').val();
-  var $newChild = $('<p>').text($newChildName);
-  $container.append($newChild);
+  var $newMovieName = $('input').val();
+  var options = {
+    url: "http://www.omdbapi.com/?apikey=2f6435d9&s=" + $newMovieName
+  };
+
+  $.ajax(options).done(function(response){
+    response.Search.forEach(function(movie){
+      var $newSectElem = $('<section>');
+      var $newH2Elem = $('<h2>');
+      var $newImageElem = $('<img>');
+
+      $newSectElem.append($newH2Elem);
+      $newSectElem.append($newImageElem);
+      $newH2Elem.text(movie.Title);
+      $newImageElem.attr('src', movie.Poster);
+      $container.append($newSectElem);
+      $('input').val().text = '';
+    });
+  });
 });
