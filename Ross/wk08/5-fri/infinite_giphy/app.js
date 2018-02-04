2afa4398fa9ee2de4f343485f5cdbcd10a3ec89f
roscolil <ross.lillis@gmail.com>
2018-02-05 07:57:49 +1100

    bug fixes/refactoring

--- Ross/wk08/5-fri/infinite_giphy/app.js
+++ Ross/wk08/5-fri/infinite_giphy/app.js
@@ -1,3 +1,4 @@
+var $inputValue;
 var gifOffset = 10;
 var $list = $('.list');
 
@@ -14,16 +15,16 @@ function ajaxGifRequest(request) {
   });
 }
 
+// Search click event
 $('button').on('click', function() {
-  var $inputValue = $('input').val();
+  $inputValue = $('input').val();
   var giphs1 = {
     url: "http://api.giphy.com/v1/gifs/search?q=" + $inputValue + "&api_key=Y7syfs6uxUSXC1AZtFb3M9BgiQyb6T3z&limit=10"
   };
+
   $('.list').empty();
   $('input').val('');
   ajaxGifRequest(giphs1);
-
-  // Searches working ok until i refactored the code and created a ajax/dom function. Having an issue with the $inputValue/gifOffset in the scroll function on subsequent searches. First 10 results are correct but it's not resetting to new value when you scroll down. I alternates old/new every 10 results.
 
   $(window).scroll(function() {
     if ($(window).scrollTop() == $(document).height() - $(window).height()) {
