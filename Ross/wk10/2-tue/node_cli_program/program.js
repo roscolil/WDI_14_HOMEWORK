8cf39e764a20b1342ea51cf0cf719dd537dfbcf5
roscolil <ross.lillis@gmail.com>
2018-02-13 16:31:15 +1100

    tried forEach method

--- Ross/wk10/2-tue/node_cli_program/program.js
+++ Ross/wk10/2-tue/node_cli_program/program.js
@@ -1,6 +1,10 @@
 var sum = 0;
-for (i = 2; i < process.argv.length; i++) {
-	sum += Number(process.argv[i]);
-}
+// for (i = 2; i < process.argv.length; i++) {
+// 	sum += Number(process.argv[i]);
+// }
+
+process.argv.slice(2).forEach(function(elem){
+  sum += Number(elem);
+});
 
 console.log(sum);
