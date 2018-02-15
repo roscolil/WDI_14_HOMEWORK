b61aee572e68ebc0ff5bd166b6a99eea5a43e36d
roscolil <ross.lillis@gmail.com>
2018-02-15 16:43:23 +1100

    formatting code

--- Ross/wk10/4-thu/sum_of_multiples/app.js
+++ Ross/wk10/4-thu/sum_of_multiples/app.js
@@ -2,10 +2,10 @@ function sumOfMultiples(num) {
   var multipleTotals = 0;
   for (var i = 0; i < num; i++)
   {
-      if (i % 3 === 0 || i % 5 === 0)
-      {
-          multipleTotals += i;
-      }
+    if (i % 3 === 0 || i % 5 === 0)
+    {
+      multipleTotals += i;
+    }
   }
   return multipleTotals;
 }
