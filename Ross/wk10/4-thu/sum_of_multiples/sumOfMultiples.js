77d6e5a205e0546fdb92acb27a948c8f830419d9
roscolil <ross.lillis@gmail.com>
2018-02-16 08:55:30 +1100

    found solution

--- Ross/wk10/4-thu/sum_of_multiples/sumOfMultiples.js
+++ Ross/wk10/4-thu/sum_of_multiples/sumOfMultiples.js
@@ -1,21 +1,11 @@
 var multipleTotals = 0;
-// var num = 0;
-// var num = process.argv[2];
+var num = process.argv[2];
 
-// Having some trouble getting the 3 or 5 to add to total. Other calculations seem to be working ok.
 
-if (process.argv[2] === 3) {
-  multipleTotals = 3;
-} else if (process.argv[2] === 5) {
-  multipleTotals = 5;
-}
-
-for (var i = 0; i < process.argv[2]; i++) {
+for (var i = 0; i <= num; i++) {
   if (i % 3 === 0 || i % 5 === 0){
     multipleTotals += i;
   }
 }
 
-
 console.log(multipleTotals);
-
