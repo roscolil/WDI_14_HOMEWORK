20629711f3b5102be5243c1c4aee1918b0890dc4
roscolil <ross.lillis@gmail.com>
2018-02-16 16:01:55 +1100

    removed api key

--- Ross/wk10/5-fri/node_weather_cli/weather.js
+++ Ross/wk10/5-fri/node_weather_cli/weather.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var location = process.argv[2];
 
-var url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=77e9766c73f97d889a41ced24f3a18ad";
+var url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=" + process.env.WEATHER_KEY;
 
 request(url, function(error, response, body){
   var output = JSON.parse(body);
