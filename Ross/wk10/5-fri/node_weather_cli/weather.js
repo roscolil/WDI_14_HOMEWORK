b355ba80b87e3de5529b14bccc65e46c87be16db
roscolil <ross.lillis@gmail.com>
2018-02-16 15:45:25 +1100

    app working

--- Ross/wk10/5-fri/node_weather_cli/weather.js
+++ Ross/wk10/5-fri/node_weather_cli/weather.js
@@ -1,9 +1,10 @@
 var request = require('request');
 var location = process.argv[2];
 
-var url = "api.openweathermap.org/data/2.5/weather?q=" + location;
+var url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=77e9766c73f97d889a41ced24f3a18ad";
 
-request(url, function(){
-
-
+request(url, function(error, response, body){
+  var output = JSON.parse(body);
+  var tempCelsius = output.main.temp - 273.15;
+  console.log(tempCelsius.toFixed(1));
 });
